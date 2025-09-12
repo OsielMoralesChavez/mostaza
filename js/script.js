621c50521c14cd9e3a8a4324bde53ac8cbc3f24e
Osiel Morales <95189472+OsielMoralesChavez@users.noreply.github.com>
2025-09-12 11:11:11 -0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// navbar-scroll.js
+document.addEventListener("DOMContentLoaded", function () {
+  const navbar = document.querySelector(".navbar");
+
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 50) {
+      navbar.classList.add("navbar-scrolled");
+    } else {
+      navbar.classList.remove("navbar-scrolled");
+    }
+  });
+});
